b594abc662baf8bded6b7a38fa2ed9836d4fa326
Chase McCarthy <chase@code0100fun.com>
2015-06-17 17:59:17 -0400

    Extract string template loader in print test

--- tests/print-test.js
+++ tests/print-test.js
@@ -6,21 +6,25 @@ import { parse } from 'handlebars/dist/cjs/handlebars/compiler/base';
 describe('print', function(){
   var baseDir = './tests/fixtures';
 
-  function parseFixture(name) {
-    const expected = fs.readFileSync(`${baseDir}/${name}.hbs`).toString('utf8');
-    const ast = parse(expected);
+  function loadFromFixture(name) {
+    const template = fs.readFileSync(`${baseDir}/${name}.hbs`).toString('utf8');
+    return loadFromString(template);
+  }
+
+  function loadFromString(template) {
+    const ast = parse(template);
     const actual = print(ast);
-    return { actual, expected };
+    return { actual, expected: template };
   }
 
   describe('it prints Handlebars AST to Handlebars template', function(){
     it('simple HTML', function(){
-      const { actual, expected } = parseFixture('simple-html');
+      const { actual, expected } = loadFromFixture('simple-html');
       assert.equal(actual, expected);
     });
 
     it('mustache', function(){
-      const { actual, expected } = parseFixture('mustache');
+      const { actual, expected } = loadFromFixture('mustache');
       assert.equal(actual, expected);
     });
   });
