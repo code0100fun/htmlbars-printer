26c887ff45f953e961ea78d891b15d39845c29e6
Chase McCarthy <chase@code0100fun.com>
2015-06-17 22:06:37 -0400

    Printer prints indented HTML/mustaches

--- tests/print-test.js
+++ tests/print-test.js
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import print from '../htmlbars-printer/print';
 import { parse } from 'handlebars/dist/cjs/handlebars/compiler/base';
 
-describe('print', function(){
+describe('print(ast)', function(){
   var baseDir = './tests/fixtures';
 
   function loadFromFixture(name) {
@@ -17,14 +17,24 @@ describe('print', function(){
     return { actual, expected: template };
   }
 
-  describe('it prints Handlebars AST to Handlebars template', function(){
+  describe('prints Handlebars AST to Handlebars template', function(){
     it('simple HTML', function(){
-      const { actual, expected } = loadFromFixture('simple-html');
+      const { actual, expected } = loadFromString('<h1>Test</h1>');
       assert.equal(actual, expected);
     });
 
     it('mustache', function(){
-      const { actual, expected } = loadFromFixture('mustache');
+      const { actual, expected } = loadFromString('{{ name }}');
+      assert.equal(actual, expected);
+    });
+
+    it('indented HTML', function(){
+      const { actual, expected } = loadFromFixture('indented-html');
+      assert.equal(actual, expected);
+    });
+
+    it('indented with mustaches', function(){
+      const { actual, expected } = loadFromFixture('indented-with-mustaches');
       assert.equal(actual, expected);
     });
   });
